489a92a1dc0103d669a05d07c33b45b34cf6dfc6
rorrorojas3 <rorro.irg@gmail.com>
2021-04-26 21:41:02 -0400

    Implemented MicrosoftLogin

--- rodrigo-tech-fitness-ui/src/components/signInModal/signInModal.js
+++ rodrigo-tech-fitness-ui/src/components/signInModal/signInModal.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Modal, Button, Grid, Row, Col } from "react-bootstrap";
 import { GoogleLogin } from "react-google-login";
 import FacebookLogin from "react-facebook-login";
+import MicrosoftLogin from "react-microsoft-login";
 
 const SignInModal = (props) => {
   const responseGoogle = (response) => {
@@ -10,6 +11,10 @@ const SignInModal = (props) => {
 
   const responseFacebook = (response) => {
     console.log(response);
+  };
+
+  const authHandler = (err, data) => {
+    console.log(err, data);
   };
 
   return (
@@ -46,6 +51,12 @@ const SignInModal = (props) => {
               scope="public_profile,email"
               callback={responseFacebook}
               icon="fa-facebook"
+            />
+          </Row>
+          <Row className="justify-content-center mt-2">
+            <MicrosoftLogin
+              clientId="e6a0ca00-d569-44d1-a41e-2fa6b08b59a0"
+              authCallback={authHandler}
             />
           </Row>
         </Modal.Body>
